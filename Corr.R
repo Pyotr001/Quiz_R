23f03cabc7738d42f6c8d8157bee985ffb04f721
Pyotr001 <pyotr001@gmail.com>
2015-03-04 00:46:26 +0300

    corr - почти работает

--- Corr.R
+++ Corr.R
@@ -11,13 +11,19 @@ corr <- function(directory = "specdata", threshold = 0) {
                         }
                 }
                 table <- read.csv(paste(directory, "/", f_name, sep = ""))
-                Check <- sum(!is.na(table$sulfate & !is.na(table$nitrate)))
+                Check <- sum(!is.na(table$sulfate) & !is.na(table$nitrate))
                 if(Check > threshold){
-                        outputVector <- c(outputVector, cor(table$sulfate, table$nitrate, na.rm = TRUE)
+                        dataCor <- cor(table$sulfate, table$nitrate, use = "na.or.complete")
+                        # print(dataCor)
+                        outputVector <- c(outputVector, dataCor)
+                        # print(head(table$sulfate))
+                        # print(outputVector)
                 }
         }
-                outputVector   
                 
+        
+        outputVector 
+        #print(length(outputVector))
 }
        
 
