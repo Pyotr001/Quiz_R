7efbed107802f503505e4604881a241dd128d312
Pyotr001 <pyotr001@gmail.com>
2015-03-03 15:52:36 +0300

    first commit

---
+++ Quiz1.R
@@ -0,0 +1,15 @@
+# читаем таблицу
+table <- read.csv("hw1_data-2.csv")
+
+# Extract the first 2 rows of the data frame and print them to the console. What does the output look like?
+head(table, 2)
+
+# How many observations (i.e. rows) are in this data frame?
+nrow(table)
+
+# Extract the last 2 rows of the data frame and print them to the console. What does the output look like?
+tail(table, 2)
+
+# What is the value of Ozone in the 47th row?
+Ozone$47
+
