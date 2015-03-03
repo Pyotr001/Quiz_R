a985293f5bc609f108e78637278866a3d75fa355
Pyotr001 <pyotr001@gmail.com>
2015-03-03 20:30:55 +0300

    Второе задание

---
+++ complete.R
@@ -0,0 +1,37 @@
+complete <- function(directory = "specdata", id = 1:332){
+        c <- c()
+       
+        for(i in id){
+                if(i[1] < 10){
+                        f_name <- paste("00", i, ".csv", sep = "")
+                }else{
+                        if(i[1] < 100){
+                                f_name <- paste("0", i, ".csv", sep = "") 
+                        }else{
+                                f_name <- paste(i, ".csv", sep = "") 
+                        }
+                }
+                
+                table <- read.csv(paste(directory, "/", f_name, sep = ""))
+                Check <- sum(!is.na(table$sulfate & !is.na(table$nitrate)))
+                
+                c <- rbind(c, c(i, Check))
+        }
+        print(c)
+}
+        
+        
+        
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return a data frame of the form:
+        ## id nobs
+        ## 1  117
+        ## 2  1041
+        ## ...
+        ## where 'id' is the monitor ID number and 'nobs' is the
+        ## number of complete cases
