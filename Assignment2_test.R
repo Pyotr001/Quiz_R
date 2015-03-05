b438136d81a57d35407d4561d56669797661e215
Pyotr001 <pyotr001@gmail.com>
2015-03-05 15:51:08 +0300

    Assignment 2 work

--- Assignment2_test.R
+++ Assignment2_test.R
@@ -1,18 +1,26 @@
 makeVector <- function(x = numeric()) {
         m <- NULL
-        set <- function(y) {
+        set <- function(y) { 
                 x <<- y
-                m <<- NULL
+                m <<- NULL # очищаем кеш при новой записи
         }
         get <- function() x
         setmean <- function(mean) m <<- mean
         getmean <- function() m
-        list(set = set, get = get,
+        list(set = set, 
+             get = get, # Создаем список из функций обращаемся
              setmean = setmean,
              getmean = getmean)
 }
+# Создаем список с функциями, куда можно сразу передать вектор исходных значений
+# x <- makeVector(c(1, 2, 3, 45, 7))
+# x$get() выдает исходный вектор
+# x$setmean("...") кеширует то, что передаём
+# x$getmean() отдает кеш
 
 
+# cachemean(x) сосчитает среднее и покажет его и запишет в кэш
+# при наличии в кеше - вдаеет предупреждающую строку
 cachemean <- function(x, ...) {
         m <- x$getmean()
         if(!is.null(m)) {
@@ -24,3 +32,5 @@ cachemean <- function(x, ...) {
         x$setmean(m)
         m
 }
+
+ 
