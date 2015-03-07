b70eefd47016fa7b9de6c99d60ae930645d23458
Pyotr001 <pyotr001@gmail.com>
2015-03-07 23:42:44 +0300

    Assignment3.3 V1.0
    Не работало условие отбора неправильных. Отбирал отсутствующие значения

--- rankhospital.R
+++ rankhospital.R
@@ -5,7 +5,8 @@ rankhospital <- function(state = "MD", outcome = "pneumonia", num = "best") {
                           colClasses = "character")
         table <- table[, c(2,7,11,17,23)]
         names(table) <- c("name", "state", "HeartAttack", "HeartFailure", "Pneumonia")
-        suppressWarnings(table[3:5] <- apply(table, 2, as.numeric))
+        print(names(table))
+        # suppressWarnings(table[3:5] <- apply(table, 2, as.numeric))
         
         ## Check that state and outcome are valid
         if ((sum((unique(table[2]))  == state)) == 0){
@@ -26,14 +27,21 @@ rankhospital <- function(state = "MD", outcome = "pneumonia", num = "best") {
                 }
         }
         
-        ## print(head(outcome.data))
+        print(head(outcome.data))
         ## print(length(outcome.data[[1]]))
         ## убираем строчки NA
         outcome.data <- outcome.data[!is.na(outcome.data[2]), ]
+        print(head(outcome.data))
+        
+        ## убираем пропущенные значения
+        print(length(outcome.data[ ,2]))
+        outcome.data <- outcome.data[outcome.data[ ,2] != "Not Available", ]
+        print(length(outcome.data[ ,2]))
         
         ## сортируем в начале по значению, потом по имени
         outcome.data <- outcome.data[order(outcome.data[2], outcome.data[1]), ]
-        
+        print(head(outcome.data))
+        print(tail(outcome.data))
         if(num == "best"){
                 num <- 1
         }else{
