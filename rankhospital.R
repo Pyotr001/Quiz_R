503395c6aa7eef31c31e17a74cc562883930c31d
Pyotr001 <pyotr001@gmail.com>
2015-03-07 15:07:49 +0300

    Version,1 - не работает

--- rankhospital.R
+++ rankhospital.R
@@ -1,15 +1,49 @@
-source('~/Documents/Документы/Самообразование/Data Science/R Programming/Quiz_R/Assingment3_read_data.R')
 
 rankhospital <- function(state = "MD", outcome = "pneumonia", num = "best") {
         ## Read outcome data
-        table <- data.frame()
-        read_data()
+        table <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv",
+                          colClasses = "character")
+        table <- table[, c(2,7,11,17,23)]
+        names(table) <- c("name", "state", "HeartAttack", "HeartFailure", "Pneumonia")
+        suppressWarnings(table[3:5] <- apply(table, 2, as.numeric))
         
         ## Check that state and outcome are valid
-        # if ((sum((unique(table[2]))  == state)) == 0) stop("invalid state")
-                
+        if ((sum((unique(table[2]))  == state)) == 0){
+                stop("invalid state")
+        }
         
+        if (outcome == "pneumonia"){
+                outcome.data <- table[which(table[2] == state), c(1, 5)]
+        }else{
+                if(outcome == "heart attack"){
+                        outcome.data <- table[which(table[2] == state), c(1, 3)]  
+                }else{
+                        if(outcome == "heart failure"){
+                                outcome.data <- table[which(table[2] == state), c(1, 4)]    
+                        }else{
+                                stop("invalid outcome")
+                        }
+                }
+        }
+        
+        ## print(head(outcome.data))
+        ## print(length(outcome.data[[1]]))
+        ## убираем строчки NA
+        outcome.data <- outcome.data[!is.na(outcome.data[2]), ]
+        
+        ## сортируем в начале по значению, потом по имени
+        outcome.data <- outcome.data[order(outcome.data[2], outcome.data[1]), ]
+        
+        if(num == "best"){
+                num <- 1
+        }else{
+                if(num == "worst"){
+                        num <- nrow(outcome.data)
+                }
+        }
+        
+        outcome.data[num, 1]
+        ## length(outcome.data[[1]])        
                 
-                
-                print(head(table))
+                # print(head(table))
 }
